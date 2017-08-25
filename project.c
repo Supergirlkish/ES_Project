3cc52f56f191bca67ee7d6db9ed3f2608c4240ac
drnobodyphd <rchase@ltu.edu>
2017-08-25 17:12:08 -0400

    added toggle switches

--- project.c
+++ project.c
@@ -1,6 +1,10 @@
 #include "project.h"
 #include <stdio.h>
 #include <stdint.h>
+
+//Momentary switches are connected to
+//
+
 //*****************************************************************************
 //
 // The error routine that is called if the driver library encounters an error.
@@ -15,50 +19,69 @@ __error__(char *pcFilename, uint32_t ui32Line)
 #endif
 
 
+void PlayArea ()
+{
+	int32_t scratch;
+	scratch = GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4|GPIO_PIN_0);
+	scratch = ~scratch & (GPIO_PIN_4|GPIO_PIN_0) ; 
+	switch (scratch)
+	{
+		case GPIO_PIN_4:
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2,0xF);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,0);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,0);
+			break;
+		case GPIO_PIN_0:
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,0xF);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,0);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2,0);
+			break;
+		case (GPIO_PIN_4|GPIO_PIN_0):
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,0);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,0xF);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2,0);
+			break;
+		default:
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,0);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3,0);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2,0);
+			break;
+	}
+}
+
+//call this periodically to update services
+void ServiceRoutine()
+{
+		UpdateSwitches();
+}
+
+
 void SetupHardware()
 {
 	UartSetup();
-//	UartSetup2();
+	//UartSetup2();  //alternate version
+	GpioSetup();
+	//GpioSetup2();  //alternate version
+	
 }
 
 int  main(void)
 {
 		uint8_t temp;
     volatile uint32_t ui32Loop;
-	
-    // Enable the GPIO port that is used for the on-board LED.
-		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    SetupHardware();
 		
-		// Check if the peripheral access is enabled.
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
-    {
-    
-		}
-    
-		// Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+		EnableAllPorts();					//make sure ports are enabled. Cant configure unenabled port
+		SetupHardware();
+		
     while(1)
     {
 				UARTCharPut(UART0_BASE, temp);
 			  temp++;
-				//UARTCharPut(UART0_BASE, '\n');
-				//UARTCharPut(UART0_BASE, '\r');
-        // Turn on the LED.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0xF);
+				UARTCharPut(UART0_BASE, '\n');
+				UARTCharPut(UART0_BASE, '\r');
 
-        // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
+				PlayArea();
+				ServiceRoutine();
 
-        // Turn off the LED.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0x0);
-
-        // Delay for a bit.
-        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
-        {
-        }
     }
 }
